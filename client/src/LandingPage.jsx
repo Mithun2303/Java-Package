86efc4336bc3965557a5c530afd8f2bb9c450328
Mithun@2303 <mithunkarthick1610@gmail.com>
2024-04-01 18:44:50 +0530

    Change of DB

---
+++ client/src/LandingPage.jsx
@@ -0,0 +1,9 @@
+export const LandingPage = () => {
+    return(
+        <main>
+            <span className="text-3xl">
+            hello
+            </span>
+        </main>
+    )
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/movie_review");
+db = mongoose.connection;
+
+
+db.on("error",(error)=>{
+    console.log(error)
+})
+
+db.once("open",()=>{
+    console.log("Connected to db");
+})
 
 app.use(cors());
 app.use(express.json())
